791cb4dcf83cdc0c7f26ea05a3c6bb28da042ab8
Drew Miller <drew.miller@mrjaffesclass.com>
2023-10-20 12:29:28 -0700

    added a few more cases

--- arraylist-scanner/Main.java
+++ arraylist-scanner/Main.java
@@ -1,13 +1,17 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello User!");
     ArrayList<Integer> arrayList = new ArrayList<>();
+    for (int i = 4; i >= 0; i--) {
+      arrayList.add(i);
+    }
     System.out.println(arrayList);
     Scanner userInput = new Scanner(System.in);
     boolean editList = true;
-    System.out.println("add to list: a, remove from index: r, remove first occurence of an integer: rf; remove all occurences of an integer: ra, quit program: q, print list: p");
+    System.out.println("add to list: a, remove from index: r, sort list: s, clear list: c, remove first occurence of an integer: rf; remove all occurences of an integer: ra, quit program: q, print list: p");
     while (editList) {
       System.out.println("-----------");
       try {
@@ -17,6 +21,12 @@ class Main {
             System.out.println("enter number you wish to add");
             int addToList = userInput.nextInt();
             arrayList.add(addToList);
+            break;
+          case "c":
+            arrayList.clear();
+            break;
+          case "s":
+            arrayList.sort(Comparator.naturalOrder());
             break;
           case "r":
             System.out.println("enter the index of the integer you wish to remove");
@@ -45,10 +55,9 @@ class Main {
             System.out.println(arrayList);
             break;
           default:
-            System.out.println("Uknown Input");
+            System.out.println("ArrayList: " + arrayList + "");
             break;
         }
-        System.out.println("ArrayList: " + arrayList + "");
       } catch (Exception e) {
         System.out.println("Invalid Input");
         userInput.next();
